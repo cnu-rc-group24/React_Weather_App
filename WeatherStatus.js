8d9a8956e9be19e7e045f05f701be23825370ddb
Kim HyunSeop <hyunseop95@gmail.com>
2019-06-14 22:41:29 +0900

    Add WeatherStatus.js
    날씨 정보를 한글로 변경하기 위한 JS파일, 관련 문구, 아이콘 정보 등을 가져옴

---
+++ WeatherStatus.js
@@ -0,0 +1,74 @@
+
+export const weatherStatus = {
+  Rain: {
+    color: '#7DBAD9',
+    title: '비',
+    subtitle: '우산을 챙겨주세요.',
+    subtitle2: "가끔은 비오는 소리도 들어봐요.",
+    subtitle3: "이런 날 커피 한 잔 아시죠?",
+    icon: 'weather-rainy'
+  },
+
+  Clear: {
+    color: '#f9db61',
+    title: '맑음',
+    subtitle: '선크림은 필수입니다.',
+    subtitle2: "오늘 하루 행복하게!",
+    subtitle3: "물 한 잔 잊지마세요.",
+    icon: 'weather-sunny'
+  },
+
+  Thunderstorm: {
+    color: '#616161',
+    title: '비바람',
+    subtitle: '창문을 잘 닫아주세요.',
+    subtitle2: "쌀쌀할지도 모르니 외투준비!",
+    subtitle3: "우산은 챙겼나요?",
+    icon: 'weather-lightning-rainy'
+  },
+
+  Clouds: {
+    color: '#1F1C2C',
+    title: '구름',
+    subtitle: '하늘이 다소 가려졌어요.',
+    subtitle2: "흐린날 다음은 맑은 날이!",
+    subtitle3: "오늘 구름이 많네요!",
+    icon: 'weather-cloudy'
+  },
+
+  Snow: {
+    color: '#e5faff',
+    title: '눈',
+    subtitle: '모자를 꾹꾹 눌러 써주세요.',
+    subtitle2: "덜덜덜~ 따뜻하게 입으세요.",
+    subtitle3: "따뜻한 코코아 한잔 어떠세요?",
+    icon: 'weather-snowy'
+  },
+
+  Drizzle: {
+    color: '#076585',
+    title: '가랑비',
+    subtitle: '곧 그칠 거에요.',
+    subtitle2: "빨래는 걷으셨나요?",
+    subtitle3: "우산은 챙기셨죠?",
+    icon: 'weather-hail'
+  },
+
+  Haze: {
+    color: '#90BACC',
+    title: '연무',
+    subtitle: "오후에도 흐릴 수 있어요.",
+    subtitle2: "가끔은 흐린날도 좋잖아요?.",
+    subtitle3: "안전운전 하세요!",
+    icon: 'weather-hail'
+  },
+
+  Mist: {
+    color: '#3a3a3a',
+    title: '안개',
+    subtitle: '저속운전을 해주세요.',
+    subtitle2: "다시 날이 풀릴 거에요.",
+    subtitle3: "안전운전 하세요!",
+    icon: 'weather-fog'
+  }
+};
